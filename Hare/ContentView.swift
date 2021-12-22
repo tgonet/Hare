071fd343fa025ba7d171f5b387a9041d514f4ebe
Ming Zhe <tanmingzhe.99@gmail.com>
2021-12-22 16:10:04 +0800

    added maps

--- Hare/ContentView.swift
+++ Hare/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world")
-            .padding()
-    }
-}
+  @State private var region = MKCoordinateRegion(
+    // Apple Park
+    center: CLLocationCoordinate2D(latitude: 37.334803, longitude: -122.008965),
+    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+  )
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  @State private var lineCoordinates = [
+
+    // Steve Jobs theatre
+    CLLocationCoordinate2D(latitude: 37.330828, longitude: -122.007495),
+
+    // Caffè Macs
+    CLLocationCoordinate2D(latitude: 37.336083, longitude: -122.007356),
+
+    // Apple wellness center
+    CLLocationCoordinate2D(latitude: 37.336901, longitude:  -122.012345)
+  ];
+
+  var body: some View {
+    MapView(
+      region: region,
+      lineCoordinates: lineCoordinates
+    )
+      .edgesIgnoringSafeArea(.all)
+  }
 }
